fc0f2d00bf17276f9e3ce5b45e94c8f4b9bb3745
Qi Xin (Chi Hsin) <xinqi0511@gmail.com>
2021-05-27 21:50:44 -0400

    from LIF to GLM
    make W exponential

--- LIFtoGLM.m
+++ LIFtoGLM.m
@@ -46,13 +46,13 @@ xlabel('Mean Voltage');
 ylabel('Firing rate');
 title({'Mean Integrated Squared Error';'between True Stimulus and Normalized PSTH'});
 
-%%
+%% Exactly happen to be exponential 
 p = 5e-3;
 q = 0;
 tot_t = 1e8;
 bin = 1;
 ddt = bin;
-V_E = 1e3;
+V_E = 2e-1;
 V_I = 0;
 V_th = 1e8;
 V_reset = 0;
@@ -62,7 +62,7 @@ tau_M = 1e2;
 dt = 1;
 [~,~,~,V,inputE,inputI] = GetISI(tau_E,tau_I,tau_M,V_E,V_I,p,q,1e8,V_reset,0*ones(1,tot_t),tot_t,dt,-1);
 figure
-h = histogram(V,5e1);
+h = histogram(V,5e1,'Normalization','pdf');
 yy = h.Values;
 xx = h.BinEdges;
 %xx = cumsum(xx);
@@ -83,6 +83,95 @@ title('Exp tail');
 % subplot(2,2,2)
 % plot(f2,xx,yy);
 % title('Gaussian tail');
+% subplot(2,2,3)
+% plot(f3,xx,yy);
+% title('1/x tail');
+
+%% 1/x
+p = 0;
+q = 0;
+tot_t = 1e8;
+bin = 1;
+ddt = bin;
+V_E = 2e0;
+V_I = 0;
+V_th = 1e8;
+I = zeros(1,tot_t);
+I(1:5e4:end) = V_E;
+V_reset = 0;
+tau_E = 1e-3;
+tau_I = 0e-8;
+tau_M = 1e2;
+dt = 0.1;
+[~,~,~,V,inputE,inputI] = GetISI(tau_E,tau_I,tau_M,V_E,V_I,p,q,1e8,V_reset,I,tot_t,dt,-1);
+figure
+h = histogram(V,5e1,'Normalization','pdf');
+yy = h.Values;
+xx = h.BinEdges;
+%xx = cumsum(xx);
+%yy = cumsum(yy);
+start = 5;
+endnum = 0;
+xx= xx(start+1:end-endnum)';
+yy = yy(start:end-endnum)';
+[f1,gof1] = fit(xx(1:end),yy(1:end),'exp1');
+%[f2,gof2] = fit(xx(1:3),yy(1:3),'gauss1');
+ft = fittype('b/(c*x+a)','independent','x','dependent','y');
+[f3,gof3] = fit(xx,yy,ft);
+
+figure
+% subplot(2,2,1)
+plot(f1,xx,yy);
+title('Exp tail');
+% subplot(2,2,2)
+% plot(f2,xx,yy);
+% title('Gaussian tail');
+% subplot(2,2,3)
+plot(f3,xx,yy);
+title('1/x tail');
+
+figure;
+plot(V(1:2e5));
+
+
+%% Gaussian
+p = 5e-2;
+q = 0;
+tot_t = 1e8;
+bin = 1;
+ddt = bin;
+V_E = 1e-1;
+V_I = 0;
+V_th = 1e8;
+V_reset = 0;
+tau_E = 1e-3;
+tau_I = 1e-8;
+tau_M = 1e2;
+dt = 1;
+[~,~,~,V,inputE,inputI] = GetISI(tau_E,tau_I,tau_M,V_E,V_I,p,q,1e8,V_reset,0*ones(1,tot_t),tot_t,dt,-1);
+figure
+h = histogram(V,5e1,'Normalization','pdf');
+yy = h.Values;
+xx = h.BinEdges;
+%xx = cumsum(xx);
+%yy = cumsum(yy);
+start = 30;
+endnum = 0;
+xx= xx(start+1:end-endnum)';
+yy = yy(start:end-endnum)';
+[f1,gof1] = fit(xx(1:end),yy(1:end),'exp1');
+[f2,gof2] = fit(xx(1:3),yy(1:3),'gauss1');
+ft = fittype('b/(c*x+a)','independent','x','dependent','y');
+[f3,gof3] = fit(xx,yy,ft);
+
+figure
+hold on
+% subplot(2,2,1)
+plot(f1,xx,yy);
+title('Exp tail');
+% subplot(2,2,2)
+plot(f2,xx,yy);
+title('Gaussian tail');
 % subplot(2,2,3)
 % plot(f3,xx,yy);
 % title('1/x tail');
