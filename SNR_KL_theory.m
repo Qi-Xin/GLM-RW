b9143e77b3d2444060255e488f8d189738b95627
Qi Xin <qix@andrew.cmu.edu>
2021-08-09 15:36:18 -0400

    KL variance

--- SNR_KL_theory.m
+++ SNR_KL_theory.m
@@ -1,37 +1,46 @@
 clearvars;
 
-addpath(genpath('D:/Github/GLM-RW'))
-
-repnum = 3e3;
+t_tot = 1e3;
 maxSig = 7e-2;
 minSig = 2e-2;
-signalType = 3; % 1 for no signal, 2 for square wave, 3 for gamma, 4 for Gaussian random
-dt = 1e-3;
-I_per = zeros(1,1e3);
-if signalType == 2
-    I_per(301:410) = 1;
-end
-if signalType == 3
-    pd = makedist('InverseGaussian','mu',2e2,'lambda',6e2);
-    I_per(201:1000) = pdf(pd,[1:800]);
-end
-if signalType == 4
-    I_per = abs(normrnd(0.05,0.03,1,1e3));
-end
-if signalType == 5
-    I_per = repmat([ones(1,50),zeros(1,50)],1,10);
-end
-I_per = I_per/max(I_per)*(maxSig-minSig) + minSig;
-%plot(I_per);
-I = repmat(I_per,repnum,1);
-FRmulit = I;
-ST = poissrnd(FRmulit);
-FR = I_per;
 
-meanFR = mean(FR);
-SNR_direct = sum((FR-meanFR).^2*dt)/meanFR;
-SNR_indirect = var(mean(ST)) / mean(var(ST)); 
-SNR_indirect_baseline = var(mean(ST(:,201:800))) / mean(var(ST(:,[1:200,801:1000])));
-SNR_indirect_Lesica = var(mean(ST)) / mean( var( (ST-mean(ST))' )' );
-db_true = 10*log10(SNR_direct);
-db_sq = 10*log10(SNR_indirect);
+sum_range = 10;
+EPEfr_v = zeros(1,25);
+EPEconst_v = zeros(1,25);
+EPEfr_kl = zeros(1,25);
+EPEconst_kl = zeros(1,25);
+
+j = 0;
+width_range = 10:10:250;
+for width = width_range
+    j = j + 1;
+    pd = makedist('Normal','mu',t_tot/2,'sigma',width);
+    normalcurve = pdf(pd,[1:1e3]);
+    fr = minSig*ones(1,t_tot) + (maxSig-minSig)*normalcurve/max(normalcurve);
+    meanfr = mean(fr)*ones(1,t_tot);
+
+    for i = 0:sum_range
+        EPEfr_v(j) = EPEfr_v(j) + sum( fr.^i.*exp(-fr)./factorial(i) .* (i-fr).^2 );
+        EPEconst_v(j) = EPEconst_v(j) + sum( fr.^i.*exp(-fr)./factorial(i) .* (i-meanfr).^2 );
+        EPEfr_kl(j) = EPEfr_kl(j) + sum( fr.^i.*exp(-fr)./factorial(i) .* log(fr.^i.*exp(-fr)./factorial(i)) );
+        EPEconst_kl(j) = EPEconst_kl(j) + sum( fr.^i.*exp(-fr)./factorial(i) .* log(meanfr.^i.*exp(-meanfr)./factorial(i)) );
+    end
+    
+end
+
+SNR_v = (EPEconst_v - EPEfr_v)./EPEfr_v;
+SNR_kl = (EPEconst_kl - EPEfr_kl)./EPEfr_kl;
+
+%%
+figure
+yyaxis left
+plot(width_range,SNR_kl);
+
+yyaxis right
+plot(width_range,SNR_v);
+
+legend('KL based SNR','Variance based SNR');
+xlabel('Bump width (ms)');
+%%
+figure
+plot(1:1e3,fr)
